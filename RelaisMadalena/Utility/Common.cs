243e2bff874be14b75467673a83ef600ad33ec11
Amit <ariel.amittest@gmail.com>
2021-05-17 10:55:59 +0530

    part 3

---
+++ RelaisMadalena/Utility/Common.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web.JavaScript;
+
+namespace RelaisMadalena.Utility
+{
+    public class Common
+    {
+       
+    }
+}
